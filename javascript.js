b8cc921424979c928f4838352fcc9102c41a8cb7
abooodgmaing <139368284+abooodgmaing@users.noreply.github.com>
2023-08-07 21:30:33 +0300

    created

---
+++ javascript.js
@@ -0,0 +1,26 @@
+
+
+
+
+const age = 15;
+let name;
+name = "Abdullah Alyahya"
+
+console.log(age + name)
+
+name = "Abdullah"
+console.log(age + name)
+
+//age = 16 --not possible
+
+console.log(age + name)
+
+{
+    let name2 = "Mohammed"
+}
+
+{
+    let age2 = "14"
+}
+
+//console.log(name2 + age2) not possible
